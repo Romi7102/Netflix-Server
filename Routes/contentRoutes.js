9705dde73711d53601389d70844245aba398b152
Romi Landau <romi7102@gmail.com>
2023-08-09 09:30:55 +0300

    init

---
+++ Routes/contentRoutes.js
@@ -0,0 +1,64 @@
+import express from "express";
+import Content from "../Models/ContentModel.js";
+import { isAuth } from "../utils.js";
+import expressAsyncHandler from "express-async-handler";
+
+export const contentRouter = express.Router();
+const PAGE_SIZE = 10;
+
+//! Endpoints!
+//todo:
+
+contentRouter.get(
+  "/",
+//   isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const content = await Content.find();
+    res.send(content);
+  })
+);
+
+contentRouter.get(
+  "/id/:id",
+  //   isAuth,
+  expressAsyncHandler(async (req, res) => {
+    //! no need to try catch thanks for express async handler
+    const { id } = req.params;
+    console.log(id);
+    const content = await Content.findById(id);
+    content
+      ? res.send(content)
+      : res.status(404).send({ message: "Not Found" });
+  })
+);
+
+contentRouter.get(
+  "/search",
+  // isAuth,
+  expressAsyncHandler(async (req, res) => {
+    console.log("search end point");
+    const { query } = req;
+    const pageSize = PAGE_SIZE;
+    const page = query.page || 1;
+    const searchQuery = query.query || "";
+
+    const queryFilter = searchQuery
+      ? { title: { $regex: searchQuery, $options: "i" } }
+      : {};
+    const contents = await Content.find({
+      ...queryFilter,
+    })
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
+    const countContent = await Content.countDocuments({
+      ...queryFilter,
+    });
+    console.log(countContent);
+    res.send({
+      contents,
+      page,
+      countContent: countContent,
+      pages: Math.ceil(countContent / pageSize),
+    });
+  })
+);
---
+++ index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./Routes/userRoutes.js";
+import { seedRouter } from "./Routes/seedRoutes.js";
+import { contentRouter } from "./Routes/contentRoutes.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));  //todo check what this is
+
+app.use("/api/users", userRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/content", contentRouter);
+
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    console.log("Connected to Mongodb!");
+    app.listen(PORT);
+    console.log("server listening on port " + PORT);
+  })
+  .catch((e) => console.log(e));
